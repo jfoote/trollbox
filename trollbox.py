a8018a66ed399333a1873fc2fee536fe79019b79
Jonathan Foote <jmfoote@loyola.edu>
2014-12-04 15:48:18 -0500

    changed layout to separate selection and search controls

--- trollbox.py
+++ trollbox.py
@@ -20,29 +20,39 @@ class MainWindow(QMainWindow):
         self.tagEdit.setPlaceholderText("Current Image Tags")
         self.urlEdit = QLineEdit(centralWidget)
         self.urlEdit.setPlaceholderText("Current Image URL")
-        liveCheckBox = ClearableCheckBox("Live Tag Search", centralWidget)
-        saveButton = QPushButton("Download/Save Changes", centralWidget)
-        deleteButton = QPushButton("Delete Image", centralWidget)
-        clearButton = QPushButton("Clear Searches", centralWidget)
+        saveButton = QPushButton("Save Selection", centralWidget)
+        deleteButton = QPushButton("Delete Selection", centralWidget)
+
+        searchEdit = QLineEdit(centralWidget)
+        searchEdit.setPlaceholderText("Image Tag Search")
+        liveCheckBox = ClearableCheckBox("Keylogger Search", centralWidget)
         imagePicker = ImagePicker(centralWidget)
+
+        getUrlEdit = QLineEdit(centralWidget)
+        getUrlEdit.setPlaceholderText("Download Image URL")
+        getUrlButton = QPushButton("Get URL", centralWidget)
 
         # Set layout
         layout = QGridLayout(centralWidget)
-        layout.addWidget(imagePicker, 0, 0, 2, 19)
-        layout.addWidget(self.urlEdit, 20, 0)
-        layout.addWidget(self.tagEdit, 20, 1)
-        layout.addWidget(liveCheckBox, 20, 2)
-        layout.addWidget(saveButton, 21, 0)
-        layout.addWidget(clearButton, 21, 1)
-        layout.addWidget(deleteButton, 21, 2)
+        layout.addWidget(imagePicker, 0, 0, 4, 19)
+        selectionBox = QVBoxLayout()
+        selectionBox.addWidget(self.urlEdit)
+        selectionBox.addWidget(self.tagEdit)
+        selectionBox.addWidget(saveButton)
+        selectionBox.addWidget(deleteButton)
+        layout.addLayout(selectionBox, 0, 20)
+        layout.addWidget(searchEdit, 5, 0)
+        layout.addWidget(liveCheckBox, 5, 1)
+        layout.addWidget(getUrlEdit, 6, 0)
+        layout.addWidget(getUrlButton, 6, 1)
 
         # Let user search by tag and URL
-        self.tagEdit.textChanged.connect(imagePicker.setFilterTagsString)
-        self.urlEdit.textChanged.connect(imagePicker.setFilterUrl)
+        #self.tagEdit.textChanged.connect(imagePicker.setFilterTagsString)
+        #self.urlEdit.textChanged.connect(imagePicker.setFilterUrl)
 
         # When user picks an image, filter out others
-        imagePicker.selectedTagsStringChanged.connect(self.tagEdit.setText)
-        imagePicker.selectedUrlChanged.connect(self.urlEdit.setText)
+        #imagePicker.selectedTagsStringChanged.connect(self.tagEdit.setText)
+        #imagePicker.selectedUrlChanged.connect(self.urlEdit.setText)
 
         # - save changes to tag edit box in model
         #self.tagEdit.textEdited.connect(imagePicker.setTagsString)
